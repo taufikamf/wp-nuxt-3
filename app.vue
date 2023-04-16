f2e99fa77ed6cafaed9a780b3c0426dc7ab6f3bf
taufikamf <taufikabdul354@gmail.com>
2023-04-16 14:49:20 +0700

    add all code

---
+++ app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <section v-if="showContent">
+      <NuxtWelcome/>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    showContent() {
+      // check if the user came from Facebook
+      const referrer = document.referrer.toLowerCase()
+      return !referrer.includes('facebook')
+    }
+  },
+  mounted() {
+    // redirect to an external URL if the user came from Facebook
+    const referrer = document.referrer.toLowerCase()
+    if (referrer.includes('facebook')) {
+      window.location.replace('https://thedramaclubs.com/')
+    }
+  }
+}
+</script>
