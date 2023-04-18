400a6bddf4297d09d24fb6815baeead2dc7e6eab
taufikamf <taufikabdul354@gmail.com>
2023-04-18 17:44:37 +0700

    finishing

--- app.vue
+++ app.vue
@@ -8,6 +8,17 @@
 
 <script>
 export default {
+  data(){
+    return{
+      domain: 'https://thedramaclubs.com/',
+      data: {},
+      slug: '',
+      link: ''
+    }
+  },
+  async created(){
+    
+  },  
   computed: {
     showContent() {
       // check if the user came from Facebook
@@ -17,11 +28,20 @@ export default {
       }
     }
   },
-  mounted() {
+  async mounted() {
     // redirect to an external URL if the user came from Facebook
     const referrer = document.referrer.toLowerCase()
     if (referrer.includes('facebook')) {
-      window.location.replace('https://thedramaclubs.com/')
+      if(this.slug == '/'){
+        window.location.href = 'https://thedramaclubs.com/'
+      }else if(this.slug != '/'){
+        this.slug = this.$route.path
+        const slug = ref(this.slug)
+        const { data, pending, error, refresh } = await useFetch('https://thedramaclubs.com/wp-json/wp/v2/posts',{
+          query: { slug }
+        })
+        window.location.href = data.value[0].link
+      }
     }
   }
 }
